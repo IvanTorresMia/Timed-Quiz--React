d8bd605e0f7662037b592fff4b00822f4ea1ca9c
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-25 02:21:09 -0800

    simple footer

---
+++ src/Component/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+
+function Footer () {
+
+    return (
+        <div className="footer p-2 fixed-bottom">
+        <div className="row">
+          <div className="col-lg text-center">
+            <h1 className="display-4">Footer</h1>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Footer;
