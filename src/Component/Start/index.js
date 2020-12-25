d05a4b153084c34f26630eb24cbceae9535a7164
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-25 02:19:55 -0800

    Working on the start container

---
+++ src/Component/Start/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './style.css'
+
+function Start() {
+
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    How Much do you know?
+                </div>
+                <div className="row">
+                    <button className="btn">
+                        Start
+                    </button>
+                </div>
+                <div className="row">
+                    Timed Quiz
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Start
