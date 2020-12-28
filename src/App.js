f480a6cc4b95fef62c494c101ff5d6e2a2056e12
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-27 21:22:49 -0800

    setting up quiz components and making footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-import Home from './Pages/Home'
+import Home from './Pages/Home';
+import CreateQuiz from './Pages/CreateQuiz';
+import TakeQuiz from './Pages/TakeQuiz';
+import NavBar from './Components/NavBar'
+import Footer from './Components/Footer'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
+  <Router>
     <div className="App">
-     
+      <NavBar />
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/CreateQuiz" component={CreateQuiz}/>
+      <Route exact path="/TakeQuiz" component={TakeQuiz}/>
+      <Footer />
     </div>
+  </Router>
   );
 }
 
--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -4,13 +4,14 @@ import './style.css'
 function Footer () {
 
     return (
-        <div className="footer p-2 fixed-bottom">
-        <div className="row">
-          <div className="col-lg text-center">
-            <h1 className="display-4">Times Quiz</h1>
+      <footer className="footer top-line">
+      <div className="my-footer navbar-dark bg-dark">
+          <div className="footer-copyright text-center py-3 text-muted">
+              © 2020 Copyright:
+              <a href="https://choosealicense.com/licenses/mit/">MIT licenses</a>
           </div>
-        </div>
       </div>
+  </footer>
     )
 }
 
